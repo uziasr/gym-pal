71157603ba30162974ccf6676708114b4ec01048
Uzias Rivera <uziasrivera@gmail.com>
2020-05-18 12:54:16 -0700

    cleaned up unused code

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// import Navigator from './routes/Navigator'
-// import MyTabs from './routes/TabNavigator'
 import  Navigator from './routes/homeStack'
-// import Navigator from './routes/homeStack'
-
-
-import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
     <>
     <Navigator/>
-    {/* <MyTabs/> */}
-    {/* <Tabs/> */}
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
