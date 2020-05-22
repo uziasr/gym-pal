6e0e2d21255dc5deeb899c8b03fb0657791f728b
Uzias Rivera <uziasrivera@gmail.com>
2020-05-21 21:24:14 -0700

    added workout drop down

--- screens/stats/Dashboard.js
+++ screens/stats/Dashboard.js
@@ -21,14 +21,16 @@ const Dashboard = ({ navigation }) => {
         total_workouts: 0
     })
 
+    const [allWorkouts, setAllWorkouts] = useState([])
+
     const [dropActive, setDropActive] = useState({
         exercises: false,
-        workout: false
+        workouts: false
     })
 
     const [serverCalled, setServerCalled] = useState({
         exercises:false,
-        workout: false
+        workouts: false
     })
 
     const styles = StyleSheet.create({
@@ -92,6 +94,16 @@ const Dashboard = ({ navigation }) => {
     }
     const dropDownHandler = (name) =>{
         setDropActive({...dropActive, [name]: !dropActive[name] })
+        if (name == 'workouts' && serverCalled[name] == false){
+            axios.get(`http://192.168.1.3:5000/user/1/${name}`)
+            .then(res => {
+                setAllWorkouts([...res.data])
+                setServerCalled(()=>{
+                    return {...serverCalled, [name]:true}
+                })
+            })
+            .catch(err => console.log(err))
+        }
     }
 
     const pressHandler = (exercise) => {
@@ -100,32 +112,50 @@ const Dashboard = ({ navigation }) => {
 
     return (
         <View style={styles.rootView}>
-            <View style={{ marginVertical: 15, color: 'white', justifyContent: 'center' }}>
-                <Text style={styles.title}>{dashData.total_workouts} Total Workout{dashData.total_workouts ? 's' : ''}!</Text>
-                <ContributionGraph
-                    values={[{ date: '2020-01-01', count: 0 }, ...getExerciseFrequencyByDate(dashData.dates)]}
-                    endDate={new Date()}
-                    numDays={105}
-                    height={220}
-                    chartConfig={chartConfig}
-                    width={screenWidth}
-                />
-            </View>
-            <View style={{ width: '100%' }}>
-                <View>
-                    <TouchableOpacity onPress={()=>dropDownHandler('exercises')} style={{ paddingHorizontal:15, display:'flex', flexDirection:'row', justifyContent:'space-between', alignContent:'center', alignItems:'center' }}>
-                        <Text style={{ fontSize: 24, color: 'white' }}>Analyze By Exercise</Text>
-                        <AntDesign name={!dropActive.exercises?"caretdown":"caretup"} size={24} color="white" />
-                    </TouchableOpacity>
-                    {dropActive.exercises ? dashData.exercises.map(exercise => (
-                        // <View style={styles.exercisesView}>
-                        <TouchableOpacity style={styles.exercisesView} onPress={() => pressHandler(exercise)} key={exercise.id}>
-                            <Text>{exercise.name}</Text>
-                        </TouchableOpacity>
-                        // </View>
-                    )) : null}
+            <ScrollView>
+                <View style={{ marginVertical: 15, color: 'white', justifyContent: 'center' }}>
+                    <Text style={styles.title}>{dashData.total_workouts} Total Workout{dashData.total_workouts ? 's' : ''}!</Text>
+                    <ContributionGraph
+                        values={[{ date: '2020-01-01', count: 0 }, ...getExerciseFrequencyByDate(dashData.dates)]}
+                        endDate={new Date()}
+                        numDays={105}
+                        height={220}
+                        chartConfig={chartConfig}
+                        width={screenWidth}
+                    />
                 </View>
-            </View>
+                <View style={{ width: '100%' }}>
+                    <ScrollView>
+                        <TouchableOpacity onPress={()=>dropDownHandler('exercises')} style={{ paddingBottom:5, marginBottom:5, paddingHorizontal:15, display:'flex', flexDirection:'row', justifyContent:'space-between', alignContent:'center', alignItems:'center', borderBottomColor:'white', borderBottomWidth:0.5  }}>
+                            <Text style={{ fontSize: 24, color: 'white'}}>My Exercises</Text>
+                            <AntDesign name={!dropActive.exercises?"caretdown":"caretup"} size={24} color="white" />
+                        </TouchableOpacity>
+                        {dropActive.exercises ? dashData.exercises.map(exercise => (
+                            // <View style={styles.exercisesView}>
+                            <TouchableOpacity style={styles.exercisesView} onPress={() => pressHandler(exercise)} key={exercise.id}>
+                                <Text>{exercise.name}</Text>
+                            </TouchableOpacity>
+                            // </View>
+                        )) : null}
+                    </ScrollView>
+                </View>
+                {/* Could be its own component! */}
+                <View style={{ width: '100%' }}>
+                    <ScrollView>
+                        <TouchableOpacity onPress={()=>dropDownHandler('workouts')} style={{ paddingBottom:5, marginBottom:5, paddingHorizontal:15, display:'flex', flexDirection:'row', justifyContent:'space-between', alignContent:'center', alignItems:'center', borderBottomColor:'white', borderBottomWidth:0.5  }}>
+                            <Text style={{ fontSize: 24, color: 'white'}}>My Workouts</Text>
+                            <AntDesign name={!dropActive.workouts?"caretdown":"caretup"} size={24} color="white" />
+                        </TouchableOpacity>
+                        {dropActive.workouts ? allWorkouts.map(( workout, index )=> (
+                            // <View style={styles.exercisesView}>
+                            <TouchableOpacity style={styles.exercisesView} key={workout.id}>
+                                <Text>Workout {index + 1}</Text>
+                            </TouchableOpacity>
+                            // </View>
+                        )) : null}
+                    </ScrollView>
+                </View>
+            </ScrollView>
         </View>
     );
 };
