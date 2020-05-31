4a0ca4c2080a8e948a64b9aa2448cd0a55422f8c
Uzias Rivera <uziasrivera@gmail.com>
2020-05-30 20:17:09 -0700

    centralized DashBoard styling

--- styles/index.js
+++ styles/index.js
@@ -1,8 +1,8 @@
 import { StyleSheet } from 'react-native'
 
 export const splitStyles = StyleSheet.create({
-    root:{
-        backgroundColor:'#2d2d2d',
+    root: {
+        backgroundColor: '#2d2d2d',
         height: '100%'
     },
     bodyWrap: {
@@ -39,13 +39,61 @@ export const splitStyles = StyleSheet.create({
         padding: 10,
         borderRadius: 10
     },
-    muscleWrap:{
-        display:'flex', 
-        flexDirection:'row', 
-        justifyContent:'space-between', 
-        alignItems:'center', 
-        alignContent: 'center', 
-        padding: 15, 
+    muscleWrap: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        alignContent: 'center',
+        padding: 15,
         margin: 5,
+    }
+})
+
+export const DashBoardStyles = StyleSheet.create({
+    title: {
+        fontSize: 24,
+        color: 'white',
+        textAlign: 'center'
+    },
+    rootView: {
+        backgroundColor: '#2d2d2d',
+        flexDirection: 'column',
+        // justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        backgroundColor: '#2d2d2d'
+    },
+    exercisesView: {
+        width: '98%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderRadius: 15,
+        marginVertical: 5,
+        backgroundColor: 'white'
+    },
+    contributionTitleWrap:{
+        marginVertical: 15, 
+        color: 'white', 
+        justifyContent: 'center'
+    },
+    statsDropDownStyle:{
+        paddingBottom: 5, 
+        marginBottom: 5, 
+        paddingHorizontal: 15, 
+        display: 'flex', 
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        alignContent: 'center', 
+        alignItems: 'center', 
+        borderBottomColor: 'white', 
+        borderBottomWidth: 0.5 
+    },
+    statsTitleStyle:{
+        fontSize: 24, 
+        color: 'white'
     }
 })
