33ee5f05e7b4b6c95d696ffa907c55bc5526e0a0
Uzias Rivera <uziasrivera@gmail.com>
2020-07-02 23:51:27 -0700

    added Activity

--- screens/workout/Workout.js
+++ screens/workout/Workout.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, ScrollView, ActivityIndicator } from 'react-native';
 import { Button } from 'react-native-elements';
 import { workoutStyles } from '../../styles/index'
 import { useSelector, useDispatch, shallowEqual } from "react-redux"
@@ -20,33 +20,37 @@ const Workout = ({ navigation }) => {
     }
 
     const completeHandler = () => {
-        dispatch(completeWorkout(state.reducer.token,state.workoutReducer.workoutId))
+        dispatch(completeWorkout(state.reducer.token, state.workoutReducer.workoutId))
         navigation.navigate("Overall Stats")
 
     }
 
+    const RecordedWorkout = () => (
+        <ScrollView>
+            {state.workoutReducer.currentWorkout.length !== 0 ? state.workoutReducer.currentWorkout.map((exercise, index) => {
+                return <View key={index} style={workoutStyles.exerciseWrapper}>
+                    <View style={workoutStyles.exerciseTextWrap}>
+                        <Text style={workoutStyles.exerciseText}>{exercise.exercise}</Text>
+                    </View>
+                    <View style={workoutStyles.setWrapper}>
+                        {exercise.sets.map((aSet, index) => {
+                            return <View key={index} style={{ ...workoutStyles.setTextWrapper, backgroundColor: aSet.unit == "pounds" ? "dodgerblue" : "green", marginBottom: index + 1 == exercise.sets.length ? 0 : 8 }}>
+                                <Text style={workoutStyles.setText}>{aSet.weight} X {aSet.repetition} ({aSet.unit == "pounds" ? "LBS" : "KG"})</Text>
+                            </View>
+                        })}
+                    </View>
+                    <View style={{ ...workoutStyles.exerciseTextWrap, paddingLeft: 10 }}>
+                        <Text style={workoutStyles.exerciseText}>{index + 1}</Text>
+                    </View>
+                </View>
+            }) :
+                null}
+        </ScrollView>
+    )
+
     return (
         <View style={workoutStyles.root}>
-            <ScrollView>
-                {state.workoutReducer.currentWorkout.length !== 0 ? state.workoutReducer.currentWorkout.map((exercise, index) => {
-                    return <View key={index} style={workoutStyles.exerciseWrapper}>
-                        <View style={workoutStyles.exerciseTextWrap}>
-                            <Text style={workoutStyles.exerciseText}>{exercise.exercise}</Text>
-                        </View>
-                        <View style={workoutStyles.setWrapper}>
-                            {exercise.sets.map((aSet, index) => {
-                                return <View key={index} style={{ ...workoutStyles.setTextWrapper, backgroundColor: aSet.unit == "pounds" ? "dodgerblue" : "green", marginBottom: index + 1 == exercise.sets.length ? 0 : 8 }}>
-                                    <Text style={workoutStyles.setText}>{aSet.weight} X {aSet.repetition} ({aSet.unit == "pounds" ? "LBS" : "KG"})</Text>
-                                </View>
-                            })}
-                        </View>
-                        <View style={{ ...workoutStyles.exerciseTextWrap, paddingLeft: 10 }}>
-                            <Text style={workoutStyles.exerciseText}>{index + 1}</Text>
-                        </View>
-                    </View>
-                }) :
-                    null}
-            </ScrollView>
+            {state.workoutReducer.loading ? <ActivityIndicator size="large" color="white" /> : <RecordedWorkout/>}
             <Button onPress={() => nextExerciseHandler()} title="Next Exercise" buttonStyle={{ backgroundColor: "green", marginVertical: 5 }} />
             <Button onPress={() => completeHandler()} title="Complete Workout" buttonStyle={{ backgroundColor: "dodgerblue" }} />
         </View>
